f774cb9cfe3ff0014e8a4d9c0e74f8b66eca0944
hyelimchoi1223 <hlchoi1223@gmail.com>
2020-11-25 16:22:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Connection_From_Web_To_App
+ Connect from Web to Window Application
---
+++ ReceiveSignal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ReceiveSignal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            byte[] buff = new byte[1024];
+            while (true)
+            {
+                TcpClient tc = listener.AcceptTcpClient();
+
+                NetworkStream stream = tc.GetStream();
+
+                byte[] outbuf = new byte[1024];
+                int nbytes = stream.Read(outbuf, 0, outbuf.Length);
+                string output = Encoding.ASCII.GetString(outbuf, 0, nbytes);
+                if (output == "stop")
+                {
+                    Console.WriteLine($"{nbytes} bytes: {output}");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"{nbytes} bytes: {output}");
+                }
+                stream.Close();
+                tc.Close();
+            }
+        }
+    }
+}
