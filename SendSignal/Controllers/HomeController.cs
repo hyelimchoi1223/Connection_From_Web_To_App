49774a0deb1734803c9bad2298e10c3760de4b00
hyelimchoi1223 <hlchoi1223@gmail.com>
2020-11-26 12:14:33 +0900

    Add : Send, Stop Button, Send Signal

--- SendSignal/Controllers/HomeController.cs
+++ SendSignal/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -32,6 +34,30 @@ namespace SendSignal.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public string Send()
+        {
+            TcpClient tc = new TcpClient("127.0.0.1", 7000);
+            string msg = "send";
+            byte[] buff = Encoding.ASCII.GetBytes(msg);
+            NetworkStream stream = tc.GetStream();
+            stream.Write(buff, 0, buff.Length);
+            stream.Close();
+            tc.Close();
+
+            return "send";
+        }
+        public string Stop()
+        {
+            TcpClient tc = new TcpClient("127.0.0.1", 7000);
+            string msg = "stop";
+            byte[] buff = Encoding.ASCII.GetBytes(msg);
+            NetworkStream stream = tc.GetStream();
+            stream.Write(buff, 0, buff.Length);
+            stream.Close();
+            tc.Close();
+
+            return "stop";
         }
     }
 }
