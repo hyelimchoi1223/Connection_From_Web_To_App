1b96c5e7637ae62e5c49e8197bbdc980a4f35d5a
hyelimchoi1223 <hlchoi1223@gmail.com>
2020-11-26 16:58:09 +0900

    Add : Send Message module (TCP)

---
+++ SendSignal/Connection/Tcp.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace SendSignal.Connection
+{
+    public class Tcp
+    {
+        private TcpClient tc;
+        public  Tcp(string Ip, int port)
+        {
+            tc = new TcpClient(Ip, port);
+        }
+        public bool Send(string message)
+        {
+            bool IsSuccess = false;
+            if(tc == null)
+            {
+                return IsSuccess;
+            }
+            byte[] buff = Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = tc.GetStream();
+            stream.Write(buff, 0, buff.Length);
+            stream.Close();
+            tc.Close();
+
+            IsSuccess = true;
+
+            return IsSuccess;
+        }
+    }
+}
