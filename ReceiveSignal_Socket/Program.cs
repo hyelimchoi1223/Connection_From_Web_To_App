ae6186fa2b7571ea805dbca1b1738671d941264c
hyelimchoi1223 <hlchoi1223@gmail.com>
2020-11-26 17:03:11 +0900

    Add : Server (Socket-TCP)

---
+++ ReceiveSignal_Socket/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ReceiveSignal_Socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(severTcpSocket);
+            thread.IsBackground = true;
+            thread.Start();
+
+            Thread.Sleep(500);
+
+            Console.WriteLine("종료하려면 아무키나 누르세요");
+            Console.ReadLine();
+        }
+
+        private static void severTcpSocket(object obj)
+        {
+            using (Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7000);
+                serverSocket.Bind(endPoint);
+
+                serverSocket.Listen(10);
+
+                while(true)
+                {
+                    Socket clientSocket = serverSocket.Accept();
+
+                    byte[] outbuf = new byte[1024];
+                    int nbytes = clientSocket.Receive(outbuf);
+                    string output = Encoding.ASCII.GetString(outbuf, 0, nbytes);
+                    Console.WriteLine($"{nbytes} bytes: {output}");
+                }
+            }
+        }
+    }
+}
